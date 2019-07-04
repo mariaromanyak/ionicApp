8d66bb7f3a7669d91214c3fc50b35abab73732f8
Andriy Jakymiv <ajakymiv@softjourn.com>
2019-07-04 10:37:20 +0300

    Fixed project structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'signin', loadChildren: './signin/signin.module#SigninPageModule' }
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", loadChildren: "./pages/login/login.module#LoginPageModule" },
+  {
+    path: "signin",
+    loadChildren: "./pages/signin/signin.module#SigninPageModule"
+  },
+  {
+    path: "welcome",
+    loadChildren: "./pages/welcome/welcome.module#WelcomePageModule"
+  }
 ];
 @NgModule({
   imports: [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
-import { Welcome } from '../pages/welcome/welcome';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
-
 export class AppComponent {
-  rootPage:string = "welcome";
-
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
   }
